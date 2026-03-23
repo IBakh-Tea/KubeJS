138666b8d578ccbf34bfa6d880a43367803a9716
Ilnar Bakhtiyarov <ilnar.16-02@mail.ru>
2026-03-23 10:05:56 +0400

    update mods and scripts

---
+++ kubejs/startup_scripts/attributeRegistry.js
@@ -0,0 +1,32 @@
+const $RangedAttribute = Java.loadClass(
+  "net.minecraft.world.entity.ai.attributes.RangedAttribute",
+);
+const ATTRIBUTE = Java.loadClass(
+  "net.minecraft.core.registries.BuiltInRegistries",
+).ATTRIBUTE;
+const $ResourceLocation = Java.loadClass(
+  "net.minecraft.resources.ResourceLocation",
+);
+const PLAYER = Java.loadClass("net.minecraft.world.entity.EntityType").PLAYER;
+
+StartupEvents.registry("minecraft:attribute", (e) => {
+  e.createCustom("humanity", () =>
+    new $RangedAttribute(
+      "attribute.name.kubejs.humanity",
+      100.0,
+      0.0,
+      100.0,
+    ).setSyncable(true),
+  );
+});
+
+NativeEvents.onEvent(
+  Java.loadClass(
+    "net.neoforged.neoforge.event.entity.EntityAttributeModificationEvent",
+  ),
+  (e) => {
+    const humanityHolder = ATTRIBUTE.getHolder("kubejs:humanity").get();
+
+    e.add(PLAYER, humanityHolder);
+  },
+);
--- kubejs/startup_scripts/keyRegistry.js
+++ kubejs/startup_scripts/keyRegistry.js
@@ -1,8 +1,10 @@
-const $RegisterKeyMappingsEvent = Java.loadClass(
-  "net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent",
-);
 const $keyMapping = Java.loadClass("net.minecraft.client.KeyMapping");
 
-NativeEvents.onEvent($RegisterKeyMappingsEvent, (event) => {
-  event.register(new $keyMapping("key.dash", 82, "key.group"));
-});
+NativeEvents.onEvent(
+  Java.loadClass(
+    "net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent",
+  ),
+  (event) => {
+    event.register(new $keyMapping("key.dash", 82, "key.group"));
+  },
+);
