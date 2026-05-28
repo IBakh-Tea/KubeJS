687a280fa817f30dfbacd8b0e08204a94bbc5ae9
Ilnar Bakhtiyarov <ilnar.16-02@mail.ru>
2026-05-28 19:45:50 +0400

    add new mods

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,3 +1,58 @@
+const ItemStack = Java.loadClass("net.minecraft.world.item.ItemStack");
+
+const slotMapper = {
+  5: 39, // EquipmentSlot.FEET
+  6: 38, // EquipmentSlot.LEGS
+  7: 37, // EquipmentSlot.CHEST
+  8: 36, // EquipmentSlot.HEAD
+  9: 9,
+  10: 10,
+  11: 11,
+  12: 12,
+  13: 13,
+  14: 14,
+  15: 15,
+  16: 16,
+  17: 17,
+  18: 18,
+  19: 19,
+  20: 20,
+  21: 21,
+  22: 22,
+  23: 23,
+  24: 24,
+  25: 25,
+  26: 26,
+  27: 27,
+  28: 28,
+  29: 29,
+  30: 30,
+  31: 31,
+  32: 32,
+  33: 33,
+  34: 34,
+  35: 35,
+  36: 0, // hotbar
+  37: 1, // hotbar
+  38: 2, // hotbar
+  39: 3, // hotbar
+  40: 4, // hotbar
+  41: 5, // hotbar
+  42: 6, // hotbar
+  43: 7, // hotbar
+  44: 8, // hotbar
+};
+
+PlayerEvents.inventoryChanged((e) => {
+  const { player, item, slot } = e,
+    inventory = player.getInventory();
+  console.info(slot);
+
+  if (slot == 5) {
+    inventory.setItem(slotMapper[slot], ItemStack.EMPTY);
+  }
+});
+
 ItemEvents.rightClicked("minecraft:iron_ingot", (event) => {
   const { player, level, target } = event;
 
